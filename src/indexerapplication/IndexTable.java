3010c8bee2d6a8d6801c1a5c9793e9f22f246955
jiajieli <jiajieli@10.245.237.128>
2014-10-26 22:37:11 -0400

    PRJ Version 3

---
+++ src/indexerapplication/IndexTable.java
@@ -0,0 +1,35 @@
+package indexerapplication;
+import java.awt.Graphics;
+
+import javax.swing.*;
+
+/**
+ * JPanel that shows the index table values.
+ * 
+ * @author Jiajie Li
+ * CSE 260 PRJ 2
+ * 10/25/14
+ */
+public class IndexTable extends JPanel{
+    // Index to be drawn
+    private Index index;
+    
+    // table to be drawn on the panel
+    private JTable indexTable;
+    
+    /**
+     * create an IndexTable object
+     * @param index	index to be drawn
+     */
+    public IndexTable(Index index) {
+	
+    }
+ 
+    /**
+     * paint the component of the panel
+     * @param g		graphic content to be drawn on
+     */
+    public void paintComponent(Graphics g) {
+	
+    }
+}
