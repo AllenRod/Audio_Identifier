6fcc9c316907d6a14a7106a3f35f137597db3b05
jiajieli <jiajieli@10.245.237.128>
2014-10-26 21:40:18 -0400

    PRJ 2 Version 2

---
+++ src/mainapplication/MainApplication.java
@@ -0,0 +1,57 @@
+package mainapplication;
+import javax.swing.*;
+import indexerapplication.Indexer;
+import clientapplication.Matcher;
+
+/**
+ * Main application that holds both client and indexer
+ * applications. This class also constructs GUI objects.
+ * 
+ * @author Jiajie Li
+ * CSE 260 PRJ 2
+ * 10/25/14
+ */
+public class MainApplication {
+    // indexer application
+    private Indexer indexer;
+    
+    // JPanel for indexer application
+    private JPanel indexerPanel;
+    
+    // client application
+    private Matcher matcher;
+    
+    // JPanel for matcher application
+    private JPanel matcherPanel;
+    
+    /**
+     * create a MainApplication object
+     * @param indexerApp	indexer application
+     * @param clientApp		client application
+     */
+    public MainApplication(Indexer indexerApp, Matcher clientApp) {
+	
+    }
+    
+    /**
+     * construct frame for main application
+     */
+    public void constructFrame() {
+	
+    }
+    
+    /**
+     * construct index application panel
+     */
+    public void constructIndexPanel() {
+	
+    }
+    
+    /**
+     * construct client application panel
+     */
+    public void constructClientPanel() {
+	
+    }
+
+}
